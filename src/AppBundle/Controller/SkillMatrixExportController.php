b4ca4af7d650c884c6fa84459332180f401b2d34
Pierre <piergranier@laposte.net>
2017-05-03 14:39:40 +0200

    Ajout bundle excel et export matrice exemple ok

--- src/AppBundle/Controller/SkillMatrixExportController.php
+++ src/AppBundle/Controller/SkillMatrixExportController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 
 
@@ -14,8 +14,37 @@ class SkillMatrixExportController extends Controller
 
     public function skillMatrixExportAction(Request $request)
     {
-		
-		
+		// ask the service for a excel object
+		$phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
+
+		$phpExcelObject->getProperties()->setCreator("liuggio")	//à changer
+		   ->setLastModifiedBy("Giulio De Donato")				// à changer
+		   ->setTitle("Matrice des FFO")
+		   ->setDescription("Document generated using PHP classes.")
+		   ->setKeywords("office 2005 openxml php")
+		   ->setCategory("Test result file");
+		$phpExcelObject->setActiveSheetIndex(0)
+		   ->setCellValue('A1', 'Hello')
+		   ->setCellValue('B2', 'world!');
+		$phpExcelObject->getActiveSheet()->setTitle('Matrice de compétences');
+		// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+		$phpExcelObject->setActiveSheetIndex(0);
+
+		// create the writer
+		$writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel2007');
+		// create the response
+		$response = $this->get('phpexcel')->createStreamedResponse($writer);
+		// adding headers
+		$dispositionHeader = $response->headers->makeDisposition(
+			ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+			'PhpExcelFileSample.xlsx'
+		);
+		$response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+		$response->headers->set('Pragma', 'public');
+		$response->headers->set('Cache-Control', 'maxage=1');
+		$response->headers->set('Content-Disposition', $dispositionHeader);
+
+		return $response;
     }
 
 }
--- src/AppBundle/Form/Type/SkillMatrixType.php
+++ src/AppBundle/Form/Type/SkillMatrixType.php
@@ -28,11 +28,9 @@ class SkillMatrixType extends AbstractType
                array('label_format' => 'formation.criticality',
                     'choices'  => array(
                         1 => '1',
-                        2 => '1+',
-                        3 => '2',
-                        4 => '2+',
-                        5 => '3',
-                        6 => '3+',
+                        2 => '2',
+                        3 => '3',
+                        4 => '4',
                     ),                    
                     'required' => false,
                 )
